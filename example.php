0a7b9957f7fd011aa18b3c35289441d1612b5354
tommyku <zoo1929@outlook.com>
2014-04-21 13:22:45 +0800

    initial commit

---
+++ example.php
@@ -0,0 +1,18 @@
+﻿<?php
+include_once("rssGenerator.php");
+
+$rss = new RssGenerator("Liftoff News","http://liftoff.msfc.nasa.gov/","Liftoff to Space Exploration.",strtotime("Tue, 10 Jun 2003 04:00:00 GMT"),strtotime("Tue, 10 Jun 2003 09:41:01 GMT"),array(
+	"docs" => "http://blogs.law.harvard.edu/tech/rss",
+	"generator" => "Weblog Editor 2.0",
+	"managingEditor" => "editor@example.com",
+	"webMaster" => "webmaster@example.com"
+));
+$rss->addItem(array(
+	"title" => "Star City",
+	"link" => "http://liftoff.msfc.nasa.gov/news/2003/news-starcity.asp",
+	"description" => "How do Americans get ready to work with Russians aboard the International Space Station? They take a crash course in culture, language and protocol at Russia's &lt;a href=\"http://howe.iki.rssi.ru/GCTC/gctc_e.htm\"&gt;Star City&lt;/a&gt;.",
+	"pubDate" => "Tue, 03 Jun 2003 09:39:21 GMT",
+	"guid" => "http://liftoff.msfc.nasa.gov/2003/06/03.html#item573"
+));
+
+$rss->publish();
---
+++ rssGenerator.php
@@ -0,0 +1,83 @@
+<?php
+
+class RssGenerator {
+  private $_channel = array();
+  private $_items = array();
+
+  public function __construct($title=NULL,$link=NULL,$description=NULL,$pubDate=NULL,$lastBuildDate=NULL,$otherElements=array()) {
+	date_default_timezone_set("Asia/Hong_Kong");
+
+    // necessary elements
+    $this->_channel["title"] = $title ? $this->escape($title) : "";
+    $this->_channel["link"] = $link ? $this->escape($link) : "";
+    $this->_channel["description"] = $description ? $this->escape($description) : "";
+
+    // special treatment for date
+    $this->_channel["pubDate"] = $pubDate ? date(DATE_RSS,$pubDate) : date(DATE_RSS);
+    $this->_channel["lastBuildDate"] = $lastBuildDate ? date(DATE_RSS,$lastBuildDate) : date(DATE_RSS);
+		
+    // other elements
+    foreach ($otherElements as $tag => $value) {
+      $this->_chennel[$tag] = $value;
+    }
+  }
+	
+  public function set($tag,$value) {
+    if (!in_array($tag, array("pubDate","lastBuildDate"))) {
+      $this->_chennel[$tag] = $value;
+    }
+    else {
+      // special treatment for date
+      $this->_channel[$tag] = $value ? date(DATE_RSS,$value	) : date(DATE_RSS);
+    }
+  }
+
+  public function addItem($elements=array("title"=>"no title")) { // specification demands at least a title/desc is present
+    $this->_items[] = $elements;
+  }
+	
+  public function generate() {
+    $rss = <<<EOT
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+  <channel>
+
+EOT;
+    foreach ($this->_channel as $tag => $val) {
+      $rss .= "    <".$tag.">";
+      $rss .= $val;
+      $rss .= "</".$tag.">";
+      $rss .= PHP_EOL;
+    }
+
+    foreach ($this->_items as $item) {
+      $rss .= "    <item>".PHP_EOL;
+      foreach ($item as $tag => $val) {
+	$rss .= "      <".$tag.">";
+	$rss .= $val;
+	$rss .= "</".$tag.">".PHP_EOL;
+      }
+      $rss .= "    </item>".PHP_EOL;
+    }
+
+    $rss .= <<<EOT
+  </channel>
+</rss>
+EOT;
+    return $rss;
+  }
+  
+  public function publish() {
+    ob_clean();
+    header("Content-Type: text/xml; charset=utf-8"); 
+	echo $this->generate();
+  }
+	
+  private function escape($str) {
+    return str_replace(
+		       array("\"","'","<",">","&"),
+		       array("&quot;","&apos;","&lt;","&gt;","&amp;"),
+		       $str
+		       );
+  }
+};
